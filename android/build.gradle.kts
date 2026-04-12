3ddcbfb3c6a5004e72d2e5596eaf23fa406782d6
calvin <73125365+h200137j@users.noreply.github.com>
2026-04-12 21:15:13 +0200

    build: upgrade Android Gradle plugin and Kotlin versions, update flex_color_scheme, and add subproject namespace configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -19,6 +19,21 @@ subprojects {
     project.evaluationDependsOn(":app")
 }
 
+subprojects {
+    project.plugins.withId("com.android.library") {
+        val android = project.extensions.getByName("android")
+        try {
+            val getNamespace = android.javaClass.getMethod("getNamespace")
+            val namespace = getNamespace.invoke(android)
+            if (namespace == null) {
+                val setNamespace = android.javaClass.getMethod("setNamespace", String::class.java)
+                setNamespace.invoke(android, "dev.isar.isar_flutter_libs")
+            }
+        } catch (e: Exception) {
+        }
+    }
+}
+
 tasks.register<Delete>("clean") {
     delete(rootProject.layout.buildDirectory)
 }
